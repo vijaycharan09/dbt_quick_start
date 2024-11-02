70050e301ac604fc401aa81023ed5488f3144ac0
VIJAY CHARAN BORA <vijaycharan@gmail.com>
2024-11-02 17:54:55 +0000

    cn

--- models/customers.sql
+++ models/customers.sql
@@ -16,7 +16,7 @@ customer_orders as (
         customer_id,
 
         min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
+       -- max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
 
     from orders
